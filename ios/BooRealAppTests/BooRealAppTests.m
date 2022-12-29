73766187edbf7c7884478bd367b2c6f84c4a385a
giordano scalzo <giordano.scalzo@ceracare.co.uk>
2022-12-29 19:38:05 +0000

    Renamed app

--- ios/BooRealAppTests/BooRealAppTests.m
+++ ios/BooRealAppTests/BooRealAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NavigatorAppTests : XCTestCase
+@interface BooRealAppTests : XCTestCase
 
 @end
 
-@implementation NavigatorAppTests
+@implementation BooRealAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
